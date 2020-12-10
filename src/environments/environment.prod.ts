8a5770254d27d0cb2709c5c713afd3967a15b562
Emerick95 <ahilton.guaras@epn.edu.ec>
2020-12-09 22:43:28 -0500

    Prueba Topicos-Aplicacion Agenda

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDuojjMkhW2eN7qBq-IpkBaYljWlMJehyg",
+    authDomain: "topicos-prueba.firebaseapp.com",
+    databaseURL: "https://topicos-prueba-default-rtdb.firebaseio.com",
+    projectId: "topicos-prueba",
+    storageBucket: "topicos-prueba.appspot.com",
+    messagingSenderId: "466341007401",
+    appId: "1:466341007401:web:2417e2e31e261ce858cc38",
+    measurementId: "G-6X5M93PW8M"
+  }
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDuojjMkhW2eN7qBq-IpkBaYljWlMJehyg",
+    authDomain: "topicos-prueba.firebaseapp.com",
+    databaseURL: "https://topicos-prueba-default-rtdb.firebaseio.com",
+    projectId: "topicos-prueba",
+    storageBucket: "topicos-prueba.appspot.com",
+    messagingSenderId: "466341007401",
+    appId: "1:466341007401:web:2417e2e31e261ce858cc38",
+    measurementId: "G-6X5M93PW8M"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
